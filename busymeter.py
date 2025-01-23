ae5219dd6405ad897debd0c15a7780634d3df34c
Marco Pisco <marco@marcopisco.com>
2025-01-23 21:17:22 +0000

    UNDO CHANGES TO ANOTHER PROTOTYPE BACK TO vA0.1

--- busymeter.py
+++ busymeter.py
@@ -1,15 +1,16 @@
-import bluetooth
+import asyncio
+from bleak import BleakScanner
+from operator import length_hint
 import time
 import os
 
-def lookup():
-    nearby_devices = bluetooth.discover_devices()
-    os.system('cls')
-    print("Found {} devices.".format(len(nearby_devices)))
-
-    for addr in nearby_devices:
-        print("  {}".format(addr))
+async def main():
+    os.system("cls")
+    devices = await BleakScanner.discover()
+    print(length_hint(devices))
+    for d in devices:
+        print(d)
+    time.sleep(60)
 
 while True:
-    lookup()
-    time.sleep(60)
+    asyncio.run(main())
