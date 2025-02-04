bdaf85bf08a9a5aeb8b28ccb931460732aa3895e
Marco Pisco <a11531@aejics.org>
2025-02-04 23:03:39 +0000

    Passar a usar main.py para o ficheiro de código

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from operator import length_hint
 import time
 import os
 
+os.remove("test.txt")
 async def main():
-    os.system("cls")
-    devices = await BleakScanner.discover(timeout=15,scanning_mode='active')
+    devices = await BleakScanner.discover(timeout=10,scanning_mode='active')
     for d in devices:
         print(d)
     print(length_hint(devices))
-    print("Esperar")
     time.sleep(30)
 
 while True:
